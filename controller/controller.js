a490d257ec24c1e0a6c39d611ee132fa937a7a8e
Santiago Villabona <88673966+MeperDonas@users.noreply.github.com>
2024-09-20 14:03:13 -0500

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -6,7 +6,7 @@ export const AllProducts = async (req, res) => {
         conn = await pool.getConnection();
         console.log("Iniciando la conexión");
         const rows = await conn.query("SELECT * FROM PRODUCT;");
-        console.log("Datos obtenidos: ", rows); // Verifica qué datos se están obteniendo
+        console.log("Datos obtenidos: ", rows);
         if (rows.length > 0) {
         res.json(rows);
         } else {
