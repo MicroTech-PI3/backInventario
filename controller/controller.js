b32fe5ce57314137c5ca393e60988a31309dd6d5
Santiago Villabona <88673966+MeperDonas@users.noreply.github.com>
2024-09-20 16:53:41 -0500

    verificar si elimino

--- controller/controller.js
+++ controller/controller.js
@@ -27,10 +27,10 @@ export const DeleteProduct = async (req, res) => {
         console.log("Iniciando la conexión");
         const rows = await conn.query("DELETE FROM PRODUCT WHERE ID= " + req.params.IdProduct + ";" );
         console.log("Datos obtenidos: ", rows);
-        if (rows.length > 0) {
-        res.json(rows);
+        if (rows.affectedRows > 0) {
+            res.status(200).json({ status: "Producto eliminado correctamente" });
         } else {
-        res.status(404).json({ status: "No se encontraron productos" });
+            res.status(404).json({ status: "No se encontró el producto" });
         }
     } catch (err) {
     res.status(500).json({ status: "Error en la base de datos" });
