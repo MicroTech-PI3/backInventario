953991e6b99585e407ca20edf291406bf4a7f664
Alexa020204 <heartPink303@gmail.com>
2024-10-08 11:25:09 -0500

    Employee

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-export const DB_HOST = process.env.DB_HOST;
+export const DB_HOST = process.env.DB_HOST || "microtech.icu";
 export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_PASSWORD = process.env.DB_PASSWORD || "my-password";
 export const DB_DATABASE = process.env.DB_DATABASE|| "MICROTECH";
-export const DB_PORT = process.env.DB_PORT;
+export const DB_PORT = process.env.DB_PORT || 3307;
 export const APP_HOST = process.env.APP_HOST || "localhost";
 export const APP_PORT = process.env.APP_PORT || 3000;
